6f83fb07082cad723d021d68189e17dda7bcded8
jordicido <jordi.cidoncha4@gmail.com>
2026-03-23 17:32:30 +0100

    Modificado ejercicio 10

--- Sesion3/ejercicio10UT1.py
+++ Sesion3/ejercicio10UT1.py
@@ -4,4 +4,5 @@ num2 = int(input("Ingrese otro número:\n"))
 print(f"División decimal: {num1/num2}")
 print(f"División entera: {num1//num2}")
 print(f"Resto: {num1%num2}")
+print("Programa finalizado")
 
